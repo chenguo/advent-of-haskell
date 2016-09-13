8cee61da7ea3865d22f74849e84a7ffb29926bed
Chen Guo <chen.guo.0625@gmail.com>
2016-09-13 02:31:59 -0700

    Update day 4 solution to use isPrefixOf

--- day-4/day-4.hs
+++ day-4/day-4.hs
@@ -1,4 +1,4 @@
-import Data.List (find)
+import Data.List (find, isPrefixOf)
 import Data.Maybe (fromJust)
 import Data.Digest.Pure.MD5
 import Data.ByteString.Lazy.Char8 (pack, unpack)
@@ -7,21 +7,16 @@ main :: IO()
 main = interact $ show . solve
 
 solve :: (Num a, Enum a, Show a) => String -> a
-solve input = fromJust $ find (matchHash input matchTarget) [0..]
+solve input = find' (matchHash input matchTarget) [0..]
+
+find' :: Foldable t => (a -> Bool) -> t a -> a
+find' f l = fromJust $ find f l
 
 matchTarget :: String
-matchTarget = "000000"
+matchTarget = "00000"
 
 matchHash :: Show a => String -> String -> a -> Bool
-matchHash key matchStr val = prefixMatch matchStr $ hashValue key val
-
--- check if arg1 is prefix of arg2
-prefixMatch :: String -> String -> Bool
-prefixMatch [] _ = True
-prefixMatch _ [] = False
-prefixMatch (c1:cs1) (c2:cs2)
-  | c1 == c2 = prefixMatch cs1 cs2
-  | otherwise = False
+matchHash key matchStr val = isPrefixOf matchStr $ hashValue key val
 
 md5str :: String -> String
 md5str = show . md5 . pack
